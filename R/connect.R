4794491aa14ded81de7216c741f0709dec45adac
Jacob Blanco <jayb88@gmail.com>
2021-10-31 11:39:05 +0900

    Cleanup

--- R/connect.R
+++ R/connect.R
@@ -1,42 +1,56 @@
-connect_to_server <- function(username, startpage) {
-  baseurl <- 'http://ws.audioscrobbler.com/2.0/?'
+params <- list(
+  method = 
+  
+)
 
+lastfm_api_key <- function() {
+  key <- Sys.getenv('LASTFM_KEY')
+  if (identical(key, '')) {
+    stop('LastFM API key not found. Please set env var LASTFM_KEY', call. = FALSE)
+  }
+
+  key
+}
+
+lastfm_api <- function(endpoint, startpage = 0, ...) {
+  baseurl <- 'http://ws.audioscrobbler.com/2.0/?'
+  
   params <- list(
-    method = 'user.getrecenttracks',
-    api_key = 'e38cc7822bd7476fe4083e36ee69748e',
-    user = username,
+    method = endpoint,
+    api_key = lastfm_api_key(),
     page = startpage,
     limit = 50,
-    format = 'json'
+    format = "json",
+    ...
   )
-
+  
   url <- modify_url(baseurl, query = params)
   resp <- GET(url)
-
+  
   if (resp$status_code != 200) {
     stop("Problem with calling the API - response: ", content(req))
   }
-
-  resp <- content(resp, as = "text")
-  json_response <- fromJSON(resp)
-  json_response <- json_response$recenttracks
-
   structure(
     list(
-      content = get_tracklist(json_response),
-      username = username,
-      total_pages = get_pageinfo(json_response)
+      content = fromJSON(content(resp, as = "text")),
+      method = method,
+      response = resp
     ),
     class = "lastfm_api"
   )
 }
 
-print.lastfm_api <- function(x, ...) {
-  cat("<LastFM ", x$username, ">\n", sep = "")
-  cat("<Total Pages: ", x$total_pages, ">\n", sep = "")
-
-  print(x$content)
-  invisible(x)
+get_recent_tracks <- function(username) {
+  resp <- lastfm_api('user.getrecenttracks', )
+  json_response <- resp[["content"]][["recenttracks"]]
+  json_response 
+  structure(
+    list(
+      content = get_tracklist(json_response),
+      total_pages = get_pageinfo(json_response)
+    ),
+    class = "lastfm_api"
+  )
 }
 
 get_pageinfo <- function(response) {
