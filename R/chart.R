0bdd874dbe9071ebcb067309aa249828d5ac2f4a
Jacob Blanco <jayb88@gmail.com>
2021-11-16 22:48:29 +0900

    Fix bad argument name

--- R/chart.R
+++ R/chart.R
@@ -10,9 +10,9 @@
 #'
 #' @export
 raw_chart_get_top_artists <- function(.page = 1, .n_pages = NULL) {
-  paginate(method = "chart.getTopArtists", result_node = "artists", .page = 1, .n_pages = NULL)
+  paginate(method = "chart.getTopArtists", result_node = "artists", .start_page = 1, .n_pages = NULL)
 }
 
 raw_chart_get_top_tags <- function(.page = 1, .n_pages = NULL) {
-  paginate(method = "chart.getTopTags", result_node = "tags", .start_page = 1, .n_pages = NULL)
+  paginate(method = "chart.getTopTags", result_node = "tag", .start_page = .page, .n_pages = NULL)
 }
