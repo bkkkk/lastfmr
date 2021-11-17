3c29dec48ee527f476e6fa042e8f7b5cd6effa35
Jacob Blanco <jayb88@gmail.com>
2021-11-18 08:54:44 +0900

    Add track get similar endpoint

--- R/track.R
+++ R/track.R
@@ -73,6 +73,47 @@ raw_track_get_info <- function(track = NULL, artist = NULL, mbid = NULL, autocor
   )
 }
 
+
+#' Search for similar tracks
+#'
+#' The search is performed by providing a source track by:
+#' 
+#'     1. Artist and track name OR
+#'     2. MusicBrainz ID
+#'
+#' One and only one of the above must be provided. If both are provided this
+#' defaults to using the artist and track name.
+#'
+#' If the artist and track names provided are misspelled in the search query, this endpoint
+#' returns the correct name by default. This can be overriden by calling the function
+#' with function argument `autocorrect` set to `FALSE`.
+#'
+#' The biography language is English by default but can be changed using the `lang`
+#' argument which accepts an [ISO 639-1 country code](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes).
+#'
+#' @param track Required unless mbid is provided. The track name as a string.
+#' @param artist Required unless mbid is provided. The artist name as a string.
+#' @param mbid (Optional) MusicBrainz ID for the track as a string. Can be provided instead of artist and track names
+#' @param autocorrect (Optional) Whether to correct the artist and track names. Defaults to TRUE if not provided.
+#' @param lang (Optional) The language to return the biography in.
+#'
+#' @export
+raw_track_get_similar <- function(track = NULL, artist = NULL, mbid = NULL, autocorrect = TRUE, lang = NULL) {
+  params <- sanitize_raw_track_get_info_parameters(artist, track, mbid)
+
+  lastfmr(
+    method = "track.getSimilar",
+    result_node = "similartracks",
+    artist = params[["artist"]],
+    track = params[["track"]],
+    mbid = params[["mbid"]], 
+    autocorrect = autocorrect,
+    lang = lang
+  )
+}
+
+
+#' @keywords internal
 sanitize_raw_track_get_info_parameters <- function(artist = NULL, track = NULL, mbid = NULL) {
   if (is.null(artist) && is.null(track) && is.null(mbid)) {
     abort("`raw_track_get_info` expects to get either an artist and track or the mbid. None were provided.")
@@ -91,9 +132,4 @@ sanitize_raw_track_get_info_parameters <- function(artist = NULL, track = NULL, 
   }
 
   list("artist" = artist, "track" = track, "mbid" = mbid)
-}
-
-
-raw_track_get_similar <- function() {
-  lastfmr()
 }
