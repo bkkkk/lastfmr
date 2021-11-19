f5844cc037bfc94bf9a4f4b8491eaec8ca72e25b
Jacob Blanco <jayb88@gmail.com>
2021-11-19 20:41:45 +0900

    Add raw artist search

--- R/artist.R
+++ R/artist.R
@@ -1,4 +1,23 @@
-# TODO artist.search
+#' Search for an artist by name
+#' 
+#' Returns artist matches sorted by relevance.
+#'
+#' @inherit paginated-endpoint params return
+#'
+#' @param artist Artist name as a string.
+#'
+#' @export
+raw_artist_search <- function(track, .start_page = 1, .n_pages = NULL) {
+  paginate(
+    method = "artist.search",
+    result_node = "results",
+    artist = artist,
+    .start_page = .start_page,
+    .n_pages = .n_pages
+  )
+}
+
+
 # TODO artist.getInfo
 # TODO artist.getSimilar
 # TODO artist.getTags
