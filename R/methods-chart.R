2d0dd06e0c1c921d86971a0202524bb91a061951
Jacob Blanco <jayb88@gmail.com>
2021-11-20 14:53:31 +0900

    Merge chart docs

--- R/methods-chart.R
+++ R/methods-chart.R
@@ -1,31 +1,28 @@
-#' Get list of top artists
-#'
+#' Chart Methods
+#' 
+#' @description 
+#' 
+#' Get last.fm overall chart rankings for tags, tracks, and artists.
+#' 
 #' @inheritParams paginated-endpoint
-#'
-#' @return A vector of LastFM API objects
-#'
+#' 
+#' @name chart-methods
+NULL
+
+#' @rdname chart-methods
 #' @export
 raw_chart_get_top_artists <- function(.start_page = 1, .n_pages = NULL) {
   paginate(method = "chart.getTopArtists", result_node = "artists", .start_page = .start_page, .n_pages = .n_pages)
 }
 
-#' Get list of top tags
-#'
-#' @inheritParams paginated-endpoint
-#'
-#' @return A vector of LastFM API objects
-#'
+
+#' @rdname chart-methods
 #' @export
 raw_chart_get_top_tags <- function(.start_page = 1, .n_pages = NULL) {
   paginate(method = "chart.getTopTags", result_node = "tags", .start_page = .start_page, .n_pages = .n_pages)
 }
 
-#' Get list of top tracks
-#'
-#' @inheritParams paginated-endpoint
-#'
-#' @return A vector of LastFM API objects
-#'
+#' @rdname chart-methods
 #' @export
 raw_chart_get_top_tracks <- function(.start_page = 1, .n_pages = NULL) {
   paginate(method = "chart.getTopTracks", result_node = "tracks", .start_page = .start_page, .n_pages = .n_pages)
