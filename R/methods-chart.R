0618716b69041899dc28e51943a077052956aa3d
Jacob Blanco <jayb88@gmail.com>
2021-11-20 15:00:45 +0900

    Add first chart tidy function

--- R/methods-chart.R
+++ R/methods-chart.R
@@ -11,7 +11,8 @@
 #' 
 #' @returns 
 #' 
-#' All endpoints return a list of lastfmr objects.
+#' * All raw endpoints return a list of lastfmr objects.
+#' * All tidy endpoints return a Tibble.
 #' 
 #' @name chart-methods
 NULL
@@ -20,6 +21,23 @@ NULL
 #' @export
 raw_chart_get_top_artists <- function(.start_page = 1, .n_pages = NULL) {
   paginate(method = "chart.getTopArtists", result_node = "artists", .start_page = .start_page, .n_pages = .n_pages)
+}
+
+#' @rdname chart-methods
+#' @export
+chart_get_top_artists <- function(.start_page = 1, .n_pages = NULL) {
+  raw_responses <- raw_chart_get_top_artists(.start_page, .n_pages)
+  
+  tidy_raw_response(raw_responses, function(record) {
+    tibble(
+      name = map_chr(record, "name"),
+      playcount = map_chr(record, "playcount"),
+      listeners = map_chr(record, "listeners"),
+      mbid = map_chr(record, "mbid"),
+      streamable = as.integer(map_chr(record, "streamable")),
+      url = map_chr(record, "url")
+    )
+  }, .path = "artist")
 }
 
 
